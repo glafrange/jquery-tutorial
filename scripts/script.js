982b892a425cd38d51f54aeb1ca73ac3cec46d15
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-13 13:13:08 -0400

    parts 4-6 finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,3 +1,36 @@
-$(document).ready(function() {
-  alert("jquery loaded");
-});
+    // PART5
+    
+/*$("h3").css({border: "3px solid blue"});
+$(".wrapper").css({border: "3px solid red"});
+$("#clients").css({border: "3px solid yellow"});*/
+
+
+
+    // PART6
+
+// first and last
+/*$("header nav li:first").css({border: "2px solid red"});
+$("header nav li:last").css({border: "2px solid red"});*/
+
+// first-child and last-child
+/*$("#contact ul:first-child").css({border: "2px solid red"});
+$("#contact ul:last-child").css({border: "2px solid red"});*/
+
+// odd and even
+/*$("header nav li:even").css({border: "2px solid blue"});
+$("header nav li:odd").css({border: "2px solid yellow"});*/
+
+// not
+//$("section:not('#contact')").css({border: "2px solid green"});
+
+// less than
+//$("#social-nav li:lt(3)").css({border: "2px solid blue"})
+
+// greater than
+//$("#social-nav li:gt(2)").css({border: "2px solid blue"})
+
+// attribute filters
+//$("img[alt]").css({border: "2px solid pink"})
+
+// attribute with specific value
+//$("img[alt=quote]").css({border: "2px solid purple"})
