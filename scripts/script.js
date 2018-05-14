f1a58d9727c1116673d3861cb44a317e6b41841f
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-14 11:52:52 -0400

    part 22-24 finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,36 +1,57 @@
-    // Part 19
-
-/*$(document).ready(function(){
-    function complete(){
-        alert("animation complete");
-    }
-    $("section > h2").on("click", function(){
-        $(this).animate({"width" : "500px", "height" : "100px"}, 1000, "linear", complete); 
-    });
-});*/
-
-
-
-    // Part 20
-    
-/*$(document).ready(function(){
-    $("section > h2").on("click", function(){
-        //$(this).fadeOut(2000).fadeIn(500); 
-        $(this).fadeTo(200, 0.2)
-               .fadeTo(200, 0.8)
-               .fadeTo(200, 0.2)
-               .fadeTo(200, 0.8)
-               .fadeTo(200, 0.2)
-               .fadeTo(200, 0.8);
-    });
-});*/
-
-
-
-    // Part 21
+    // Part 22
 
 $(document).ready(function(){
-    $("img[alt=map]").on("click", function(){
-        $("section > h2").toggle(1000);
+    
+    $(".slide-button-up").on("click", function(){
+        $("#lead-banner").slideToggle(1000, function(){
+            alert("animation complete");
+        });
     });
+    
+});
+
+
+
+
+    // Part 23
+
+$(document).ready(function(){
+    
+    var allQuotes = $("blockquote");
+    var currentQuote = 0;
+    
+    function changeQuote(){
+        
+        $(allQuotes[currentQuote]).fadeOut(200, function() {
+            if(currentQuote == allQuotes.length-1) {
+                currentQuote = 0;
+            } else {
+                currentQuote++;
+            }
+            
+            $(allQuotes[currentQuote]).fadeIn(200);
+        });
+        
+
+        
+    }
+    
+    var quoteTimer = setInterval(changeQuote, 3000);
+    
+});
+
+
+
+    // Part 24
+
+$(document).ready(function(){
+   
+   var items = $("#points-of-sale li");
+   
+   items.on('click', function() {
+       
+       $(this).find("p").slideToggle(500);
+       
+   })
+    
 });
