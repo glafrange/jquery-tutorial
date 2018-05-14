8832745dba571ddef78dbe8620469a84b3d30dd0
Gabriel Lafrange <gabriellafrange@gmail.com>
2018-05-14 10:51:10 -0400

    part 13-15 finished

--- scripts/script.js
+++ scripts/script.js
@@ -1,31 +1,34 @@
-    // Part 10
+    // Part 13
+    
+/*$("#social-nav").css({
+    "top": "-400px",
+    "left": "158px",
+    "opacity": "0.5",
+    "border-top": "4px solid red"
+});
+*/
 
-/*var wrapper = "<div class='wrapper'>";
-var button = $(".button");
-var wrapped = true;
+    
+    
+    // Part 14
+    
+$("header .wrapper").removeClass("wrapper");
+$("header > div").addClass("wrapper");
+
+var button = $("#lead-banner a");
 
 button[0].onclick = function(){
-    if(wrapped){
-        $("section").unwrap();
-        wrapped = false;
-        button.text("wrap");
-    } else {
-        $("section").wrapAll(wrapper);
-        wrapped = true;
-        button.text("unwrap");
-    }
-}*/
-
-
-    // Part 11
-
-/*$(".button").remove();
-$("#contact img").remove();*/
+    $("#points-of-sale").toggleClass("open");
+    return false;
+}
 
 
 
-    // Part 12
+    // Part 15
+    
+var myLis = $("#points-of-sale li");
 
-//$("#contact img").removeAttr("alt");
-//$("#contact img").attr("alt", "location");
-//console.log($("#contact img").attr("alt"));
+myLis.on("click", function(){
+    $(this).css({background: "pink"});
+    myLis.off("click");
+});
